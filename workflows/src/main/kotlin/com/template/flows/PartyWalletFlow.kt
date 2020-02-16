6a571e118823b5fbe78dae302a0d9585a97956d6
= <=>
2020-02-15 23:30:13 -0300

    Alteração nas classes

--- workflows/src/main/kotlin/com/template/flows/PartyWalletFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/PartyWalletFlow.kt
@@ -1,63 +1,61 @@
 //package com.template.flows
 //
 //import co.paralleluniverse.fibers.Suspendable
-//import com.template.contracts.BrunoCoinContract
 //import com.template.states.BrunoCoinState
-//import com.template.states.BrunoCoinTransferState
-//import net.corda.core.contracts.Command
 //import net.corda.core.contracts.StateAndRef
-//import net.corda.core.contracts.requireThat
+//import net.corda.core.contracts.StateRef
+//import net.corda.core.crypto.SecureHash
 //import net.corda.core.flows.*
 //import net.corda.core.identity.Party
-//import net.corda.core.transactions.SignedTransaction
 //import net.corda.core.transactions.TransactionBuilder
 //import net.corda.core.utilities.ProgressTracker
 //import net.corda.core.utilities.unwrap
+//import javax.swing.plaf.nimbus.State
 //
 //object PartyWalletFlow{
 //    @InitiatingFlow
 //    @StartableByRPC
-//    class CoinTransferFlow(var amount: Double, val newOwner: Party) : FlowLogic<List<StateAndRef<BrunoCoinState>>>() {
+//    class MoneyStateRequestFlow(val otherParty : Party) : FlowLogic<StateAndRef<BrunoCoinState>?>() {
 //        companion object {
-//            object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new BrunoCoin.")
-//            object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
-//            object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
-//            object GETTING_OTHER_SIGNATURES : ProgressTracker.Step("Gathering the counterparty's signature.") {
-//                override fun childProgressTracker() = CollectSignaturesFlow.tracker()
-//            }
+//            object REQUESTING_PARTNER_INPUT : ProgressTracker.Step("Requesting to partner to put inputs in the transaction")
+//            object SENDING_BACK_TO_TRANSCTION : ProgressTracker.Step("Sending back to the transaction")
 //
-//            object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
-//                override fun childProgressTracker() = FinalityFlow.tracker()
-//            }
 //
 //            fun tracker() = ProgressTracker(
-//                    GENERATING_TRANSACTION,
-//                    VERIFYING_TRANSACTION,
-//                    SIGNING_TRANSACTION,
-//                    GETTING_OTHER_SIGNATURES,
-//                    FINALISING_TRANSACTION
+//                    REQUESTING_PARTNER_INPUT,
+//                    SENDING_BACK_TO_TRANSCTION
 //            )
 //        }
 //
 //        override val progressTracker = tracker()
 //
 //        @Suspendable
-//        override fun call(): List<StateAndRef<BrunoCoinState>> {
-//            return BrunoCoinState()
+//        override fun call(): StateAndRef<BrunoCoinState>? {
+//            val otherPartySession = initiateFlow(otherParty)
+//            progressTracker.currentStep = REQUESTING_PARTNER_INPUT
+//            val otherPartyWallet = otherPartySession.receive<StateAndRef<BrunoCoinState>>().unwrap{ it }
+//            progressTracker.currentStep = SENDING_BACK_TO_TRANSCTION
+//
+//            if(otherPartyWallet.state.data){
+//                return otherPartyWallet
+//            }else{
+//                return  null
+//            }
+//
 //        }
 //    }
 //
-//    @InitiatedBy(CoinTransferFlow::class)
-//    class MoneyReferenceFlow(val otherPartySession: FlowSession) : FlowLogic<TransactionBuilder>() {
-//        override fun call(): TransactionBuilder {
+//    @InitiatedBy(MoneyStateRequestFlow::class)
+//    class MoneyStateReponseFlow(val otherPartySession: FlowSession) : FlowLogic<Unit>() {
+//        override fun call() {
 //            val listMoneyStateAndRef = serviceHub.vaultService.queryBy(BrunoCoinState::class.java).states
-//            val txBuilder = otherPartySession.receive<TransactionBuilder>().unwrap{ it }
-//            return if(listMoneyStateAndRef.isNotEmpty()){
-//                txBuilder.addInputState(listMoneyStateAndRef.single())
-//                txBuilder
+//
+//            if(listMoneyStateAndRef.isNotEmpty()){
+//                otherPartySession.send(listMoneyStateAndRef.single())
 //            }else{
-//                txBuilder
+//
 //            }
+//
 //        }
 //    }
 //}

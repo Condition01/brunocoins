277b2089c31068a16cce7e68e56feae668ead8ee
condition01 <brunofc11@gmail.com>
2020-02-17 23:55:24 -0300

    Solving the vault bug

--- contracts/src/main/kotlin/com/template/schemas/BrunoCoinTransferSchma.kt
+++ contracts/src/main/kotlin/com/template/schemas/BrunoCoinTransferSchma.kt
@@ -1,44 +1,44 @@
-package com.template.schemas
-
-import com.template.states.BrunoCoinTransferState
-import net.corda.core.identity.AbstractParty
-import net.corda.core.schemas.MappedSchema
-import net.corda.core.schemas.PersistentState
-import net.corda.core.serialization.CordaSerializable
-import net.corda.core.utilities.MAX_HASH_HEX_SIZE
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Table
-
-object BrunoCoinTransferSchema
-
-@CordaSerializable
-object BrunoCoinTransferSchemaV1 : MappedSchema(
-        schemaFamily = BrunoCoinTransferSchema.javaClass,
-        version = 1,
-        mappedTypes = listOf(BrunoCoinTransferState::class.java)
-){
-
-    override val migrationResource = "bruno.changelog-master"
-
-    @Entity
-    @Table(name = "bruno_coin_transfer_states")
-    class PersistentBrunoCoinTransferState(
-            @Column(name = "owner_name", nullable = true)
-            var owner : AbstractParty?,
-
-            @Column(name = "new_owner_name", nullable = true)
-            var newOwner : AbstractParty?,
-
-            @Column(name = "amount", nullable = false)
-            var amount : Double,
-
-            @Column(name = "owner_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
-            var ownerKeyHash : String,
-
-            @Column(name = "new_owner_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
-            var newOwnerKeyHash : String
-    ) : PersistentState(){
-
-    }
-}
+//package com.template.schemas
+//
+//import com.template.states.BrunoCoinTransferState
+//import net.corda.core.identity.AbstractParty
+//import net.corda.core.schemas.MappedSchema
+//import net.corda.core.schemas.PersistentState
+//import net.corda.core.serialization.CordaSerializable
+//import net.corda.core.utilities.MAX_HASH_HEX_SIZE
+//import javax.persistence.Column
+//import javax.persistence.Entity
+//import javax.persistence.Table
+//
+//object BrunoCoinTransferSchema
+//
+//@CordaSerializable
+//object BrunoCoinTransferSchemaV1 : MappedSchema(
+//        schemaFamily = BrunoCoinTransferSchema.javaClass,
+//        version = 1,
+//        mappedTypes = listOf(BrunoCoinTransferState::class.java)
+//){
+//
+//    override val migrationResource = "bruno.changelog-master"
+//
+//    @Entity
+//    @Table(name = "bruno_coin_transfer_states")
+//    class PersistentBrunoCoinTransferState(
+//            @Column(name = "owner_name", nullable = true)
+//            var owner : AbstractParty?,
+//
+//            @Column(name = "new_owner_name", nullable = true)
+//            var newOwner : AbstractParty?,
+//
+//            @Column(name = "amount", nullable = false)
+//            var amount : Double,
+//
+//            @Column(name = "owner_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
+//            var ownerKeyHash : String,
+//
+//            @Column(name = "new_owner_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
+//            var newOwnerKeyHash : String
+//    ) : PersistentState(){
+//
+//    }
+//}
--- contracts/src/main/kotlin/com/template/states/BrunoCoinTransferState.kt
+++ contracts/src/main/kotlin/com/template/states/BrunoCoinTransferState.kt
@@ -1,44 +1,35 @@
 package com.template.states
 
 import com.template.contracts.BrunoCoinContract
-import com.template.schemas.BrunoCoinTransferSchemaV1
 import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.CommandAndState
 import net.corda.core.contracts.ContractState
-import net.corda.core.contracts.OwnableState
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
-import net.corda.core.internal.hash
-import net.corda.core.node.ServiceHub
-import net.corda.core.schemas.QueryableState
-import net.corda.core.schemas.MappedSchema
-import net.corda.core.schemas.PersistentState
-import  kotlin.collections.Iterable
 
 @BelongsToContract(BrunoCoinContract::class)
 data class BrunoCoinTransferState(
         val owner : Party,
         val newOwner : Party,
         val amount : Double
-) : ContractState, QueryableState{
+) : ContractState /*, QueryableState*/{
     override val participants: List<AbstractParty> get() = listOf(owner,newOwner)
 
-
-    override fun generateMappedObject(schema: MappedSchema): PersistentState {
-        return when(schema){
-            is BrunoCoinTransferSchemaV1 -> {
-                BrunoCoinTransferSchemaV1.PersistentBrunoCoinTransferState(
-                        owner = owner,
-                        newOwner = newOwner,
-                        amount = amount,
-                        ownerKeyHash = owner.owningKey.hash.toString(),
-                        newOwnerKeyHash = newOwner.owningKey.hash.toString()
-                )
-            }
-            else -> throw IllegalArgumentException("Not supported schema $schema")
-        }
-    }
-
-    override fun supportedSchemas(): Iterable<MappedSchema> = listOf(BrunoCoinTransferSchemaV1)
+//
+//    override fun generateMappedObject(schema: MappedSchema): PersistentState {
+//        return when(schema){
+//            is BrunoCoinTransferSchemaV1 -> {
+//                BrunoCoinTransferSchemaV1.PersistentBrunoCoinTransferState(
+//                        owner = owner,
+//                        newOwner = newOwner,
+//                        amount = amount,
+//                        ownerKeyHash = owner.owningKey.hash.toString(),
+//                        newOwnerKeyHash = newOwner.owningKey.hash.toString()
+//                )
+//            }
+//            else -> throw IllegalArgumentException("Not supported schema $schema")
+//        }
+//    }
+//
+//    override fun supportedSchemas(): Iterable<MappedSchema> = listOf(BrunoCoinTransferSchemaV1)
 
 }
